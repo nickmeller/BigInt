d57c21486c1e018ef0a346d77188c4d5dd0959d9
Nikita Mel'nikov <nikita_melnikov@outlook.com>
2020-07-09 09:54:25 +0300

    ya ruskiy

--- main.c
+++ main.c
@@ -8,6 +8,16 @@ typedef int* data;
 const int INIT_SIZE = 64;
 const int BASE = 1000*1000*1000;
 
+void error_while_writing(char * str) {
+    fprintf(stderr, "Error while writing into file %s", str);
+    exit(EXIT_FAILURE);
+}
+
+void error_while_alloc() {
+    fprintf(stderr, "Error while allocating memory");
+    exit(EXIT_FAILURE);
+}
+
 typedef struct {
     data num;
     int sign;
@@ -15,12 +25,37 @@ typedef struct {
     ptrdiff_t tail;
 } big_int;
 
+typedef struct {
+    big_int ** links;
+    size_t tail;
+} memStack;
+memStack * ms;
+void init_stack(memStack * s) {
+    s->tail = -1;
+    s->links = malloc(sizeof(big_int *) * 10000);
+}
+
+void push_link(memStack * s, big_int * link) {
+    if (!link) {
+        error_while_alloc();
+    }
+    s->links[++(s->tail)] = link;
+}
+
+void destroy(memStack * s) {
+    for (ptrdiff_t i = s->tail; i >= 0; i--) {
+        free(s->links[i]);
+    }
+    free(s->links);
+}
+
 size_t size(big_int * a) {
     return a->tail + 1;
 }
 
 void ensure_capacity(big_int * b) {
     b->num = realloc(b->num, b->capacity * 2);
+    if (!b->num) error_while_alloc();
     b->capacity *= 2;
 }
 
@@ -61,8 +96,10 @@ void init(big_int *b,const char * xstr) {
     b->sign = 1;
     b->capacity = INIT_SIZE;
     char * str = malloc((strlen(xstr) + 1) * sizeof(char));
+    if (!str) error_while_alloc();
     strcpy(str, xstr);
     b->num = malloc(sizeof(int) * INIT_SIZE);
+    if (!b->num) error_while_alloc();
     for (ptrdiff_t i = strlen(str); i > 0; i-= 9) {
         str[i] = 0;
         push(b, atoi(i>=9 ? str + i - 9 : str));
@@ -73,6 +110,7 @@ void init(big_int *b,const char * xstr) {
 
 big_int * init_num(int a) {
     big_int * returnable = malloc(sizeof(big_int));
+    push_link(ms, returnable);
     returnable->sign = (a >= 0 ? 1 : 0);
     returnable->capacity = 1;
     returnable->tail = 0;
@@ -84,6 +122,7 @@ big_int * init_num(int a) {
 big_int * init_cpy(big_int * a) {
     if (!a) exit(EXIT_FAILURE);
     big_int * ret = malloc(sizeof(big_int));
+    push_link(ms, ret);
     ret->sign = a->sign;
     ret->num = malloc(sizeof(int) * a->capacity);
     ret->capacity = a->capacity;
@@ -92,11 +131,19 @@ big_int * init_cpy(big_int * a) {
     return ret;
 }
 
-void print(big_int * b) {
-    if (!b->sign) printf( "-");
-    printf("%d", b->tail == -1 ? 0 : back(b));
-    for (ptrdiff_t i = b->tail - 1; i >= 0; i--)
-        printf("%09d", b->num[i]);
+
+void print(big_int * b, FILE * out) {
+    if (!b) {
+        fprintf(out, "NaN");
+    }
+    if (!b->sign && back(b) != 0) {if (fprintf(out, "-") == EOF)
+        error_while_writing("");
+    }
+    if (fprintf(out,"%d", b->tail == -1 ? 0 : back(b)) == EOF)
+        error_while_writing("");
+    for (ptrdiff_t i = b->tail - 1; i >= 0; i--) {
+        if (fprintf(out, "%09d", b->num[i]) == EOF) error_while_writing("");
+    }
 }
 
 int equals(big_int * a, big_int * b) {
@@ -210,7 +257,7 @@ big_int * div2(big_int * a) {
 }
 
 big_int * sqroot(big_int * a) {
-    if (!a->sign) exit(EXIT_FAILURE);
+    if (!a->sign) return NULL;
     big_int * l = init_num(1);
     big_int * r = init_cpy(a);
     while(less(l, minus(r, init_num(1)))) {
@@ -235,46 +282,128 @@ big_int * divl(big_int * a, big_int * b) {
             r = m;
         }
     }
+    r->sign = (a->sign == b->sign);
     return r;
 }
 
-char * input_str(char * str, FILE * in) {
-    int capacity = 64;
-    str = malloc(sizeof(char) * capacity);
 
-    char ch;
+
+char * input_str(FILE * in ) {
+    int capacity = 64;
+    char * str = malloc(sizeof(char) * capacity);
+    if (!str) error_while_alloc();
+    char ch = getc(in);
     size_t sz = 0;
-    while ((ch = getc(in) != EOF) && ch != ' ' && ch != '\n') {
+    while (ch != ' '  && ch != '\n' && ch != EOF) {
         if (sz == capacity) {
             capacity *= 2;
             str = realloc(str, sizeof(char) * capacity);
+            if (!str) error_while_alloc();
         }
         str[sz++] = ch;
+        ch = getc(in);
+    }
+    if (sz == 0) {
+        fprintf(stderr, "Error while reading from input file");
+        exit(EXIT_FAILURE);
     }
     if (sz == capacity) {
         str = realloc(str, sizeof(char) * (capacity + 1));
     }
-    str[sz] = '\n';
+    str[sz] = '\0';
     return str;
 }
 
-void input_file(char * input, char * op, char * left, char * right) {
+void input_file(char * input, char * op, char ** lefts, char ** rights) {
     FILE * in = fopen(input, "r");
     if (!in) {
-        fprintf(stderr, "Error while opening file");
+        fprintf(stderr, "Error while opening file %s", input);
         exit(EXIT_FAILURE);
     }
-    fscanf(in, "%s", op);
+    lefts[0] = input_str(in);
+    if (fgets(op, 3, in) == 0) {
+        fprintf(stderr, "Error while reading file %s", input);
+        exit(EXIT_FAILURE);
+    }
+    if (!strcmp(op, "#\n")) {
+        rights[0] = NULL;
+    } else {
+        rights[0] = input_str(in);
+    }
+    fclose(in);
+}
+
+void output(char * out, char * op, char * left, char * right) {
+    FILE * output = fopen(out, "w");
+    if (!output) {
+        fprintf(stderr, "Error while opening file %s", out);
+        exit(EXIT_FAILURE);
+    }
+    big_int * res =  NULL;
+    big_int * l = malloc(sizeof(big_int));
+    big_int * r = malloc(sizeof(big_int));
+    init(l, left);
+    if (right != NULL) init(r, right);
+
+    if (!strcmp(op, "+\n")) {
+        res = plus(l, r);
+    } else if (!strcmp(op, "-\n")) {
+        invert_sign(r);
+        res = plus(l, r);
+    } else if (!strcmp(op, "*\n")) {
+        res = multi(l, r);
+    } else if (!strcmp(op, "/\n")) {
+        res = divl(l, r);
+    } else if (!strcmp(op, "%\n")) {
+        res = minus(l, divl(l, r));
+    } else if (!strcmp(op, "#") || !strcmp(op, "#\n")) {
+        res = sqroot(l);
+    } else if (!strcmp(op, "<\n")) {
+        if(fprintf(output, (less(l, r) ? "1" : "0")) != EOF) {
+            error_while_writing(out);
+        }
+    } else if (!strcmp(op, "<=\n")) {
+        if(fprintf(output, (lessOrEquals(l, r) ? "1" : "0")) != EOF) {
+            error_while_writing(out);
+        }
+    } else if (!strcmp(op, ">\n")) {
+        if(fprintf(output, (more(l, r) ? "1" : "0")) != EOF) {
+            error_while_writing(out);;
+        }
+    } else if (!strcmp(op, ">=\n")) {
+        if(fprintf(output, (!less(l, r) ? "1" : "0")) != EOF) {
+            error_while_writing(out);
+        }
+    } else if (!strcmp(op, "==\n")) {
+        if(fprintf(output, (equals(l, r) ? "1" : "0")) != EOF) {
+            error_while_writing(out);
+        }
+    } else if (!strcmp(op, "!=\n")) {
+        if(fprintf(output, (!equals(l, r) ? "1" : "0")) != EOF) {
+            error_while_writing(out);
+        }
+    } else {
+        fprintf(stderr, "Unresolved operation %s", op);
+        exit(EXIT_FAILURE);
+    }
+    print(res, output);
+    fclose(output);
 }
 
 
 int main(int argc, char ** argv) {
+    ms = malloc(sizeof(memStack));
+    init_stack(ms);
     if (argc != 3) {
         fprintf(stderr, "Expected 2 arguments" );
         exit(EXIT_FAILURE);
     }
     char * op = malloc(sizeof(char) * 3);
-    char * op1;
-    char * op2;
-    return 0;
+    char * left;
+    char * right;
+    input_file(argv[1], op, &left, &right);
+    output(argv[2], op, left, right);
+    destroy(ms);
+    free(ms);
 }
+
